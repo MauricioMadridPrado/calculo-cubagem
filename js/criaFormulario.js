354dfa59f550c79760fe062353fd523a1fbca480
Maurício Madrid Prado <101838837+MauricioMadridPrado@users.noreply.github.com>
2023-04-11 15:38:13 -0300

    Add files via upload

--- js/criaFormulario.js
+++ js/criaFormulario.js
@@ -1,4 +1,5 @@
 export function criaFormulario() {
+  const listaDeNomes = document.getElementById('lista__de__nomes')
   const nomes =  [];
   const multy = [
     {
@@ -57,16 +58,114 @@ export function criaFormulario() {
       comprimento: 25,
       peso: 35.5,
     },
+    {
+      nome: "Multy max",
+      altura: 0.5,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 10.5,
+    },
+    {
+      nome: "Multy max",
+      altura: 1,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 20.5,
+    },
+    {
+      nome: "Multy max",
+      altura: 1.2,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 30.5,
+    },
+    {
+      nome: "Multy max",
+      altura: 1.5,
+      largura: 0.21,
+      comprimento: 25,
+      peso: 35.5,
+    },
+    {
+      nome: "Titan",
+      altura: 1,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 20.5,
+    },
+    {
+      nome: "Titan",
+      altura: 1.2,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 30.5,
+    },
+    {
+      nome: "Titan",
+      altura: 1.5,
+      largura: 0.21,
+      comprimento: 25,
+      peso: 35.5,
+    },
+    {
+      nome: "Titan",
+      altura: 1.8,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 30.5,
+    },
+    {
+      nome: "Titan",
+      altura: 2,
+      largura: 0.21,
+      comprimento: 25,
+      peso: 35.5,
+    },
+    {
+      nome: "Morada",
+      altura: 1,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 20.5,
+    },
+    {
+      nome: "Morada",
+      altura: 1.2,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 30.5,
+    },
+    {
+      nome: "Morada",
+      altura: 1.5,
+      largura: 0.21,
+      comprimento: 25,
+      peso: 35.5,
+    },
+    {
+      nome: "Morada",
+      altura: 1.8,
+      largura: 0.20,
+      comprimento: 25,
+      peso: 30.5,
+    },
+    {
+      nome: "Morada",
+      altura: 2,
+      largura: 0.21,
+      comprimento: 25,
+      peso: 35.5,
+    }
   ];
 
   let i = 0;
   multy.forEach((dado) => {
     const dadosDoProduto = document.getElementById("nome__produto");
     const formulario = document.getElementById("formulario");
-    nomes.push(dado.nome);
+
     dadosDoProduto.innerHTML = `
-    <h2 class="produto__total__cubagem" id="total__cubagem">${0}</h2>
-    <h2 class="produto__total__peso" id="kilogramas">${0}</h2>
+    <h2 class="produto__total__cubagem" id="total__cubagem">Total de metros cúbicos:0</h2>
+    <h2 class="produto__total__peso" id="kilogramas">Total kilos dos produtos :0</h2>
     `;
     formulario.innerHTML += `
         <tr class="linha__produto">
@@ -81,11 +180,44 @@ export function criaFormulario() {
         </tr>
     `;
     i++;
-
-
     ;
-
   });
+  multy.forEach(  a =>{
+    if(!nomes.includes(a.nome)){
+      nomes.push(a.nome)
+    } 
+  })
 
+  
+  nomes.forEach(  nome =>{
+    listaDeNomes.innerHTML += 
+  `
+  <li class=""><a href="#${nome}" >${nome}<a></li>
+  `
+})
+
+const campoDePesquisa = document.getElementById('pesquisa')
+campoDePesquisa.addEventListener("change", (event) => {
+
+  const nomePesquisado = event.target.value;
+  if(!nomePesquisado == ''){
+  const resultadoPesquisa = nomes.filter(nome => nome == nomePesquisado)
+  listaDeNomes.innerHTML = ` `;
+  console.log(resultadoPesquisa)
+
+  resultadoPesquisa.forEach((resultado) => {
+    listaDeNomes.innerHTML += `
+  <li class=""><a href="#${resultado}" >${resultado}<a></li>
+  `;
+  });} else{
+      
+  nomes.forEach(  nome =>{
+    listaDeNomes.innerHTML += 
+  `
+  <li class=""><a href="#${nome}" >${nome}<a></li>
+  `
+})
+  }
+});
 }
 
