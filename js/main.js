6e37c545bdea7e2abbe13f8df66b6777ed6775a6
Maurício Madrid Prado <101838837+MauricioMadridPrado@users.noreply.github.com>
2023-04-17 16:26:17 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,68 @@
 import { criaFormulario } from "./criaFormulario.js";
-import { alteraValor } from "./alteraValor.js";
-import { calculaMetragemFinal } from "./calculaMetragemFinal.js";
-import { calculaOPeso } from "./calculaOPeso.js";
-import { somaOPeso } from "./somaOPeso.js";
-criaFormulario();
-alteraValor();
-calculaMetragemFinal();
-calculaOPeso();
-somaOPeso();
 
+criaFormulario();
+
+const linhas = document.querySelectorAll(".linha__produto");
+
+function capturaOBotao() {
+  let i = 0;
+
+  linhas.forEach((linha) => {
+    const contador = document.querySelector(`[data-contador${i}]`);
+    contador.addEventListener("click", (event) => {
+      let posicao = event.target.parentNode.dataset.local;
+      let quantidadeTotal = event.target.value;
+      peso(posicao, quantidadeTotal);
+      metragem(posicao, quantidadeTotal);
+      calculosFinais();
+    });
+    i++;
+  });
+}
+
+capturaOBotao();
+
+function peso(posicao, quantidade) {
+  const pegaOPeso = document.querySelector(`[data-peso${posicao}]`).innerHTML;
+  const pesoFinal = document.querySelector(`[data-kilos${posicao}]`);
+  const peso = parseFloat(pegaOPeso) * parseFloat(quantidade);
+  pesoFinal.innerHTML = peso;
+}
+function metragem(posicao, quantidade) {
+  const metragens = document.querySelectorAll(`[data-dado${posicao}]`);
+  const valorFinal = document.querySelector(`[data-total${posicao}]`);
+  let metrosCubicos = 1 * quantidade;
+  metragens.forEach((metragem) => {
+    metrosCubicos *= metragem.innerHTML;
+  });
+  valorFinal.innerHTML = metrosCubicos;
+}
+
+function calculosFinais() {
+  const pegaTodosOsPesos = document.querySelectorAll(".valor__kilos");
+  const localOndeFicaOPeso = document.getElementById("kilogramas");
+
+  const pegaTodasAsMetragens = document.querySelectorAll(".valor__total");
+  const localOndeFicaAMetragem = document.getElementById("total__cubagem");
+
+  let j = 0;
+
+  let pesoInicial = 0;
+
+  pegaTodosOsPesos.forEach((peso) => {
+    peso = peso.innerHTML;
+    let pesoFinal = (pesoInicial += parseFloat(peso));
+    localOndeFicaOPeso.innerHTML = `Total kilos dos produtos: ${pesoFinal
+      .toFixed(2)
+      .replace(".", ",")}Kg`;
+  });
+
+  let metragemInicial = 0;
+  pegaTodasAsMetragens.forEach((metragem) => {
+    metragem = metragem.innerHTML;
+    let metragemFinal = (metragemInicial += parseFloat(metragem));
+    localOndeFicaAMetragem.innerHTML = `Total de metros cúbicos: ${metragemFinal
+      .toFixed(2)
+      .replace(".", ",")}m³`;
+  });
+}
